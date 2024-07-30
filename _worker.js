17e530fb25cdbb089158a9bb4f6b06d99b546516
reza3000-i <124606070+reza3000-i@users.noreply.github.com>
2024-07-30 14:30:40 +0330

    _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '5eb262fc-5b33-400b-b7d2-e41f2ee19fac';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
